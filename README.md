29acb69d62423ee673528d952f1c76dc0a80fabe
ajaykumar787 <60085576+ajaykumar787@users.noreply.github.com>
2020-03-15 00:05:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gcp-hack-app
+ 
---
+++ src/main/java/com/efx/gcp/hack/HackLauncher.java
@@ -0,0 +1,13 @@
+package com.efx.gcp.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackLauncher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackLauncher.class, args);
+    }
+
+}
