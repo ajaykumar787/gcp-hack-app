93cc42f1abf698270bea6f4fc34483d91f3e3f77
ajaykumar787 <60085576+ajaykumar787@users.noreply.github.com>
2020-03-15 21:24:05 +0530

    Upgraded Angular Version to 8.2.14

--- src/main/java/com/efx/gcp/hack/HackLauncher.java
+++ src/main/java/com/efx/gcp/hack/HackLauncher.java
@@ -2,8 +2,10 @@ package com.efx.gcp.hack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"com.efx.gcp.hack"})
 public class HackLauncher {
 
     public static void main(String[] args) {
--- src/main/ui/src/app/app.component.ts
+++ src/main/ui/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'app';
